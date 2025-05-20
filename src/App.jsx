be41ff4ca5a1f703e00a3c2f5719dadef8485baa
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-20 20:21:37 +0200

    Add boardgames app button

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     <div className="main-app">
       <h1>Nerdy Toolbox</h1>
       <span>This page is a work in progress.</span>
+      <Button onClick={() => window.location.href="/bg-play-next"}>Go to Boardgames: Play Next</Button>
       <Button onClick={() => window.location.href="/arcs-lars"}>Go to Arcs - Lars Bots</Button>
       <Button onClick={() => window.location.href="/witch-of-fern-island-map"}>Go to Witch of Fern Island - Resource Map</Button>
     </div>
