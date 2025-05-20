43d5348fb6ab82d9e23af81a7c1e27a564d8e879
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-20 09:50:39 +0200

    Add witch of fern island button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button } from "nerdy-lib";
+import './App.css'
 
 const App = () => {
   return (
     <div className="main-app">
       <span>This page is a work in progress.</span>
       <Button onClick={() => window.location.href="/arcs-lars"}>Go to Arcs - Lars Bots</Button>
+      <Button onClick={() => window.location.href="/witch-of-fern-island-map"}>Go to Witch of Fern Island - Resource Map</Button>
     </div>
   )
 }
