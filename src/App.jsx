73b3389625352320a145f549ea20a9b74d121759
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-20 09:54:08 +0200

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 const App = () => {
   return (
     <div className="main-app">
+      <h1>Nerdy Toolbox</h1>
       <span>This page is a work in progress.</span>
       <Button onClick={() => window.location.href="/arcs-lars"}>Go to Arcs - Lars Bots</Button>
       <Button onClick={() => window.location.href="/witch-of-fern-island-map"}>Go to Witch of Fern Island - Resource Map</Button>
