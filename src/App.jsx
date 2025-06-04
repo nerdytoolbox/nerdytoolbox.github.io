2de08bbdf18a1d357adf079ffc343a37a84cef12
Vera Prinsen <vera.prinsen@nedap.com>
2025-06-04 11:24:35 +0200

    update link

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Hub title="Nerdy Toolbox">
       <div className="align-horizontal wrap">
-        <AppCard title="Boardgames - What to play next?" thumbnail={bggThumbnail} link="/play-next" type="boardgame" />
+        <AppCard title="Boardgames - What to play next?" thumbnail={bggThumbnail} link="/bg-play-next" type="boardgame" />
         <AppCard title="Arcs - Lars bots" thumbnail={arcsThumbnail} link="/arcs-lars" type="boardgame" />
         <AppCard title="Witch of Fern Island Resource Map" thumbnail={witchOfFernIslandThumbnail} link="/witch-of-fern-island-map" type="videogame" />
       </div>
