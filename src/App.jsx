786e0923e64a4f4bbd1d2d517df9057e6bd19eb5
Vera Prinsen <vera.prinsen@nedap.com>
2025-06-04 17:38:55 +0200

    Include images in the app itself

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Hub } from "nerdy-lib";
-import { arcsThumbnail, witchOfFernIslandThumbnail, bggThumbnail } from 'nerdy-lib'
+import { Hub } from 'nerdy-lib'
 import AppCard from "./components/AppCard.jsx";
 import { BOARDGAME, VIDEOGAME } from "./util/constants.js";
 
@@ -17,9 +16,9 @@ const App = () => {
   return (
     <Hub title="Nerdy Toolbox" footer={getFooter()}>
       <div className="align-horizontal wrap">
-        <AppCard title="Boardgames - What to play next?" thumbnail={bggThumbnail} link="/bg-play-next" type={BOARDGAME} />
-        <AppCard title="Arcs - Lars bots" thumbnail={arcsThumbnail} link="/arcs-lars" type={BOARDGAME} />
-        <AppCard title="Witch of Fern Island Resource Map" thumbnail={witchOfFernIslandThumbnail} link="/witch-of-fern-island-map" type={VIDEOGAME} />
+        <AppCard title="Boardgames - What to play next?" thumbnail="bggThumbnail.png" link="/bg-play-next" type={BOARDGAME} />
+        <AppCard title="Arcs - Lars bots" thumbnail="arcsThumbnail.png" link="/arcs-lars" type={BOARDGAME} />
+        <AppCard title="Witch of Fern Island Resource Map" thumbnail="witchOfFernIslandThumbnail.png" link="/witch-of-fern-island-map" type={VIDEOGAME} />
       </div>
     </Hub>
   )
